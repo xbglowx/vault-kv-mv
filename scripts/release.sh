d6341ee9fc21cd9ab191c1fbe1c31a913066da94
Brian Glogower <xbglowx@gmail.com>
2018-12-05 01:33:47 -0800

    Have release.sh create/upload sha256sum.txt

--- scripts/release.sh
+++ scripts/release.sh
@@ -5,7 +5,7 @@ trap clean EXIT
 
 function clean {
     test -z ${BUILD_DIR:+x} || rm -rf "$BUILD_DIR"
-    rm -f *.tar.gz
+    rm -f -- *.tar.gz "$SHA256SUMS"
 }
 
 if [ $# -ne 1 ]; then
@@ -16,6 +16,7 @@ fi
 RELEASE=$1
 BUILD_DIR=$(mktemp -d)
 BINARY=vault-kv-mv
+SHA256SUMS=sha256sums.txt
 GOARCH=amd64
 export GOARCH
 
@@ -42,3 +43,9 @@ for os in "${OSES[@]}"; do
         "https://uploads.github.com/repos/xbglowx/vault-kv-mv/releases/$RELEASE_ID/assets?name=$TAR_FILENAME"
 done
 
+shasum -a 256 -- *.tar.gz > "$SHA256SUMS"
+curl -XPOST \
+    -H "Authorization: token $GITHUB_TOKEN" \
+    -H "Content-Type: $(file -b --mime-type "$TAR_FILENAME")" \
+    --data-binary @"$SHA256SUMS" \
+    "https://uploads.github.com/repos/xbglowx/vault-kv-mv/releases/$RELEASE_ID/assets?name=$SHA256SUMS"
