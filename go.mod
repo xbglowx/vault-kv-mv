3a6466ac453b76009da01258a77810d1986c9fec
Brian Glogower <xbglowx@gmail.com>
2021-01-09 23:34:19 -0500

    Fix logical import

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/xbglowx/vault-kv-mv
 
 go 1.14
 
-require github.com/hashicorp/vault/api v1.0.4
+require (
+	github.com/hashicorp/vault v1.6.1
+	github.com/hashicorp/vault/api v1.0.5-0.20201001211907-38d91b749c77
+	github.com/hashicorp/vault/sdk v0.1.14-0.20201214222404-d8fffe05d2f4
+)
--- vault-kv-mv_test.go
+++ vault-kv-mv_test.go
@@ -10,7 +10,7 @@ import (
 	"github.com/hashicorp/vault/builtin/logical/pki"
 	"github.com/hashicorp/vault/builtin/logical/transit"
 	"github.com/hashicorp/vault/helper/builtinplugins"
-	"github.com/hashicorp/vault/logical"
+	"github.com/hashicorp/vault/sdk/logical"
 	"github.com/hashicorp/vault/vault"
 
 	"fmt"
