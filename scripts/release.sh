2da3def9249da16307a0fb840448643a363e6989
Brian Glogower <xbglowx@gmail.com>
2022-12-28 16:34:39 -0500

    Build for arm64 as well

--- scripts/release.sh
+++ scripts/release.sh
@@ -13,14 +13,14 @@ if [ $# -ne 1 ]; then
     exit 1
 fi
 
-RELEASE=$1
-BUILD_DIR=$(mktemp -d)
-BINARY=vault-kv-mv
-SHA256SUMS=sha256sums.txt
-GOARCH=amd64
-export GOARCH
-
 : "${GITHUB_TOKEN:?Need to set environment variable GITHUB_TOKEN}"
+
+ARCHS=("amd64" "arm64")
+BINARY=vault-kv-mv
+BUILD_DIR=$(mktemp -d)
+OSES=("linux" "darwin" "windows")
+RELEASE=$1
+SHA256SUMS=sha256sums.txt
 
 OUTPUT=$(curl -s -XPOST \
     -H "Authorization: token $GITHUB_TOKEN" \
@@ -28,22 +28,23 @@ OUTPUT=$(curl -s -XPOST \
     --data "{\"tag_name\": \"v$RELEASE\"}" \
     https://api.github.com/repos/xbglowx/vault-kv-mv/releases
 )
-RELEASE_ID=$(echo "$OUTPUT" |jq -r '.id')
 
-declare -a OSES=("linux" "darwin")
+RELEASE_ID=$(echo "$OUTPUT" | jq -r '.id')
+
 for os in "${OSES[@]}"; do
-    TAR_FILENAME="vault-kv-mv-${RELEASE}.${os}-${GOARCH}.tar.gz"
-    export GOOS=$os
-    go build -o "$BUILD_DIR/$BINARY"
-    tar -czvf "$TAR_FILENAME" -C "$BUILD_DIR" "$BINARY"
-    curl -XPOST \
-        -H "Authorization: token $GITHUB_TOKEN" \
-        -H "Content-Type: $(file -b --mime-type "$TAR_FILENAME")" \
-        --data-binary @"$TAR_FILENAME" \
-        "https://uploads.github.com/repos/xbglowx/vault-kv-mv/releases/$RELEASE_ID/assets?name=$TAR_FILENAME"
+    for arch in "${ARCHS[@]}"; do
+        TAR_FILENAME="vault-kv-mv-${RELEASE}.${os}-${arch}.tar.gz"
+        GOOS=$os GOARCH=$arch go build -o "$BUILD_DIR/$BINARY"
+        tar -czvf "$TAR_FILENAME" -C "$BUILD_DIR" "$BINARY"
+        curl -XPOST \
+            -H "Authorization: token $GITHUB_TOKEN" \
+            -H "Content-Type: $(file -b --mime-type "$TAR_FILENAME")" \
+            --data-binary @"$TAR_FILENAME" \
+            "https://uploads.github.com/repos/xbglowx/vault-kv-mv/releases/$RELEASE_ID/assets?name=$TAR_FILENAME"
+    done
 done
 
-shasum -a 256 -- *.tar.gz > "$SHA256SUMS"
+sha256sum -- *.tar.gz > "$SHA256SUMS"
 curl -XPOST \
     -H "Authorization: token $GITHUB_TOKEN" \
     -H "Content-Type: $(file -b --mime-type "$TAR_FILENAME")" \
